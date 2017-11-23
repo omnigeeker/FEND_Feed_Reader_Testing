5064c04b991a0c52eb4588078887fadf78c4292c
ElNino Wang <elninowang@gmail.com>
2017-11-23 17:36:15 +0800

    解决case之间不独立的问题

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -6,26 +6,25 @@
 /* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。
  * 我们得保证在 DOM 准备好之前他们不会被运行。
  */
-$(function() {
+$(function(){
     /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
      * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
          * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
          * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
          * 页面看看会发生什么。
         */
-        it('RSS列表是否被定义 且 长度不为0', function() {
+        it('RSS列表是否被定义 且 长度不为0', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* 
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
-        it('测试allFeeds,保证有链接', function() {
+        it('测试allFeeds,保证有链接', () => {
             for(let feed of allFeeds) {
                 expect(feed.url).toBeDefined();
                 expect(typeof feed.url).toBe("string");
@@ -33,11 +32,10 @@ $(function() {
             }
         });
 
-
         /* 
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
-        it('测试allFeeds,保证有名字', function() {
+        it('测试allFeeds,保证有名字', () => {
             for(let feed of allFeeds) {
                 expect(feed.name).toBeDefined();
                 expect(typeof feed.name).toBe("string");
@@ -46,14 +44,13 @@ $(function() {
         });
     });
 
-
     /* 写一个叫做 "The menu" 的测试用例 */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         /*
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
-        it ('测试菜单元素默认是隐藏的', function() {
+        it ('测试菜单元素默认是隐藏的', () => {
             expect($('body').length).toBe(1);
             expect($('body').attr("class")).toBe("menu-hidden");
         });
@@ -63,7 +60,7 @@ $(function() {
           * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
           * 再次点击的时候是否隐藏。
           */
-        it ('测试菜单元素点击后可以隐藏或者显示', function() {
+        it ('测试菜单元素点击后可以隐藏或者显示', () => {
             $('.menu-icon-link').trigger('click');
             expect($('body').attr("class")).toBe("");
             $('.menu-icon-link').trigger('click');
@@ -71,8 +68,11 @@ $(function() {
         });
     });
 
+    /** 因为中众所周知的VPN的原因，用VPN访问国外服务的超时判断时间调长 */
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20 * 1000;
+
     /* 写一个叫做 "Initial Entries" 的测试用例 */
-    describe('Initial Entries', function() {
+    describe('Initial Entries', () => {
         /*
          * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
          * 里面至少有一个 .entry 的元素。
@@ -82,11 +82,11 @@ $(function() {
          */
         let index = 0;
 
-        beforeEach(function(done) {
-            loadFeed(index, function() {
+        beforeEach((done) => {
+            loadFeed(index, () => {
                 done();
             });
-            console.log("beforeEach " + index);
+            // console.log("beforeEach " + index);
             index ++;
         });
 
@@ -94,8 +94,8 @@ $(function() {
          * 循环检查所有的rss是否都正常
          */
         for (let i = 0; i < allFeeds.length; i ++) {
-            it(`loadFeed 调用 rss ${i} 正常, 至少1个 .entry 元素`, function(done) {
-                console.log($('.feed').find('.entry').first().html());
+            it(`loadFeed 调用 rss ${i} 正常, 至少1个 .entry 元素`, (done) => {
+                // console.log($('.feed').find('.entry').first().html());
                 expect($('.feed').find('.entry').length).toBeGreaterThan(0);
                 done();
             });
@@ -103,24 +103,37 @@ $(function() {
     });
 
     /* 写一个叫做 "New Feed Selection" 的测试用例 */
-    describe('New Feed Selection', function() {
+    describe('New Feed Selection', () => {
         
         /*
          * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
          * 记住，loadFeed() 函数是异步的。
          */
-        let firstRssEntry0 = $('.feed').find('.entry').first().html();
+        let firstRssEntry0;
         let secondRssEntry0;
 
-        beforeEach(function(done) {
+        beforeEach((done) => {
             // 首先加载旧源 rss 0
-            loadFeed(2, function() {
-                secondRssEntry0 = $('.feed').find('.entry').first().html();
-                done();
+            firstRssEntry0 = $('.feed').find('.entry').first().html();
+            
+            loadFeed(0, () => {
+                firstRssEntry0 = $('.feed').find('.entry').first().html();
+                loadFeed(1, () => {
+                    secondRssEntry0 = $('.feed').find('.entry').first().html();
+                    done();
+                });
             });
         });
 
-        it("测试加载新源的时候内容会变", function(done) {
+        it("测试加载新源的时候内容会变", (done) => {
+            expect(firstRssEntry0).toBeDefined();
+            expect(typeof firstRssEntry0).toBe("string");
+            expect(firstRssEntry0.length).toBeGreaterThan(0);
+
+            expect(secondRssEntry0).toBeDefined();
+            expect(typeof secondRssEntry0).toBe("string");
+            expect(secondRssEntry0.length).toBeGreaterThan(0);
+
             expect(secondRssEntry0).not.toEqual(firstRssEntry0);
             done();
         });
